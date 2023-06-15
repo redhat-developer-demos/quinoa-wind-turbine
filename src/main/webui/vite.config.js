bc9e515860c97b5d0e5c7c6343e95a89a05204e2
Andy Damevin <ia3andy@gmail.com>
2023-06-15 09:38:19 +0200

    Update to Quarkus 3.1

--- src/main/webui/vite.config.js
+++ src/main/webui/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        outDir: "build"
-    },
     plugins: [react()]
 })
