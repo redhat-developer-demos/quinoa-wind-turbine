24ee94a3f5a84b521b7074ee85880377b682f95d
Kevin Dubois <kevin.dubois@redhat.com>
2023-09-26 10:29:28 +0200

    Barbie version (#8)
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * enable shaking
    
    * Enable tapping
    
    * Update Config.js
    
    * Update Config.js
    
    * change to shaking
    
    * tapping
    
    * enable shaking
    
    * Update Config.js to v1 (#6)
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js to shake
    
    * Update Config.js
    
    * Enable shaking
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Enable tapping
    
    * Update Config.js
    
    * Enable shaking
    
    * Update Config.js
    
    * Update Config.js
    
    * Enable shasking
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Update Config.js
    
    * Revert "Revert "Barbie theme""
    
    This reverts commit b093a9228d4fd778e8083271f4f7086b8b4831d1.
    
    * update barbie car
    
    ---------
    
    Co-authored-by: Alex Soto <asotobu@gmail.com>

--- src/main/webui/src/Config.js
+++ src/main/webui/src/Config.js
@@ -4,7 +4,7 @@ export const IS_TOUCH_DEVICE = (('ontouchstart' in window)
     || (navigator.maxTouchPoints > 0)
     || (navigator.msMaxTouchPoints > 0));
 
-export const TEAM_COLORS = ['#6C84A3', 'orange'];
+export const TEAM_COLORS = ['hotpink', 'orange'];
 
 // Dashboard
 export const TAP_POWER = 15; // in MW
@@ -12,10 +12,11 @@ export const NB_TAP_NEEDED_PER_USER = DEV_MODE ? 5 : 150;
 export const SHOW_TOP = 5;
 
 // Mobile app
-export const ENABLE_TAPPING = true;
-export const ENABLE_SHAKING = false; // "false" in v1, set to "true" in v2
+export const ENABLE_TAPPING = false;
+export const ENABLE_SHAKING = true; // "false" in v1, set to "true" in v2
 export const ENABLE_BLOWING = false;
 export const ENABLE_SWIPING = false;
+
 
 // LOGGING
 console.log(`devMode: ${DEV_MODE}`);
