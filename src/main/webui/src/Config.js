162d7c0d74aefab943e760b3361ca6cd27ccba9a
Natale Vinto <nvinto@redhat.com>
2024-04-17 12:35:09 +0200

    Update Config.js

--- src/main/webui/src/Config.js
+++ src/main/webui/src/Config.js
@@ -24,8 +24,8 @@ export const NB_TAP_NEEDED_PER_USER = 100;
 export const SHOW_TOP = 5;
 
 // Mobile app
-export const ENABLE_TAPPING = false;
-export const ENABLE_SHAKING = true; // 'false' in v1, set to 'true' in v2
+export const ENABLE_TAPPING = true;
+export const ENABLE_SHAKING = false; // 'false' in v1, set to 'true' in v2
 export const ENABLE_BLOWING = false;
 export const ENABLE_SWIPING = false;
 
